5985e438f2af943bc39387d0c2ee125af0aac0ac
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2023-12-24 08:09:43 +0400

    added find method

--- app.js
+++ app.js
@@ -21,4 +21,24 @@ newUser
 	.catch((error) => {
 		console.error('Error inserting user: ', error);
 	});
+
+// find a specific item in collection by passing a query as argument to find method
+User.find({ email: 'example@email.com' })
+	.then((users) => {
+		console.log('Users found: ', users);
+	})
+	.catch((error) => {
+		console.error('Error finding user: ', error);
+	});
+
+// find all users in MongoDB collection --> use find method without any query criteria
+// retrieves all documents from the db - *returns an array*
+// User.find()
+// 	.then((users) => {
+// 		console.log('Users found: ', users);
+// 	})
+// 	.catch((error) => {
+// 		console.error('Error finding user: ', error);
+// 	});
+
 module.exports = app;
