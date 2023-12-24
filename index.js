41ca8e231d1a7ef6f171e385c309bb6c3604e5d9
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2023-12-24 05:47:40 +0400

    Started new exercise - connecting to MongoDB atlas database: commented out previous index, new index code linked to newly created app.js and dbConnect.js

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const http = require('http');
+const app = require('./app');
 
-const app = express();
-const PORT = 3000;
+app.set('port', process.env.PORT ?? 3000);
 
-let todos = [
-	{ id: 1, title: 'Todo 1' },
-	{ id: 2, title: 'Todo 2' },
-	{ id: 1, title: 'Todo 3' },
-];
+const server = http.createServer(app);
 
-app.get('/todos', (req, res) => {
-	res.json(todos);
+server.on('listening', () => {
+	console.log('Listening on port' + (process.env.PORT ?? 3000));
 });
 
-app.use(bodyParser.json());
-app.listen(PORT, () => {
-	console.log(`Listening on port ${PORT}`);
-});
+// Set up for basic Node Express app
+// const express = require('express');
+// const bodyParser = require('body-parser');
+
+// const app = express();
+// const PORT = 3000;
+
+// let todos = [
+// 	{ id: 1, title: 'Todo 1' },
+// 	{ id: 2, title: 'Todo 2' },
+// 	{ id: 1, title: 'Todo 3' },
+// ];
+
+// app.get('/todos', (req, res) => {
+// 	res.json(todos);
+// });
+
+// app.use(bodyParser.json());
+// app.listen(PORT, () => {
+// 	console.log(`Listening on port ${PORT}`);
+// });
+// END
