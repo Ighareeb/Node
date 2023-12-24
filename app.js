21efe0f43c8fe68adc2222f3b20b2a6906c463f3
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2023-12-24 08:02:19 +0400

    added code to app.js to insert new user (created new User collection as well)

--- app.js
+++ app.js
@@ -5,5 +5,20 @@ const app = express();
 const dbConnect = require('./db/dbConnect');
 
 dbConnect();
+const User = require('./db/userModel');
 
+const newUser = new User({
+	email: 'example@email.com',
+	password: 'password123',
+});
+
+// insert/save the new user to the db
+newUser
+	.save()
+	.then(() => {
+		console.log('User inserted successfully');
+	})
+	.catch((error) => {
+		console.error('Error inserting user: ', error);
+	});
 module.exports = app;
