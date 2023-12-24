15f1f619d0737ab5e0493ca8923cd4810b90f343
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2023-12-24 11:26:59 +0400

    added a middleware function to log current date when user visits the port server is listening on

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-const http = require('http');
-const app = require('./app');
+//USING MIDDLEWARE FUNCTIONS
+const express = require('express');
 
-app.set('port', process.env.PORT ?? 3000);
+const app = express();
+// Middleware functions have access to the request object (req), the response object (res), and the next middleware function(variable next) in the application’s request-response cycle.
+// (note: if the current middleware function does not end the request-response cycle, it must call next() to pass control to the next middleware function. Otherwise, the request will be left hanging.)
+const loggerMiddleware = (req, res, next) => {
+	const currentTimestamp = new Date().toISOString();
+	console.log(`[${currentTimestamp}] ${req.method} ${req.url}`);
+	next(); //in this case the next function is the route handler middleware function .get
+};
+app.use(loggerMiddleware);
 
-const server = http.createServer(app);
-
-server.on('listening', () => {
-	console.log('Listening on port' + (process.env.PORT ?? 3000));
+// terminal shows [2023-12-24T07:22:08.353Z] GET / (when you go to localhost 3000) (after listen method is called*)
+//define get route
+app.get('/', (req, res) => {
+	res.send('Hello World');
 });
+//start the express server listening on specified port and gives response when get req made
+app.listen(3000, () => {
+	console.log('Server is running on port 3000');
+});
+
+// USING MongoDB
+// const http = require('http');
+// const app = require('./app');
+
+// app.set('port', process.env.PORT ?? 3000);
+
+// const server = http.createServer(app);
+
+// server.on('listening', () => {
+// 	console.log('Listening on port' + (process.env.PORT ?? 3000));
+// });
 
 // Set up for basic Node Express app
 // const express = require('express');
